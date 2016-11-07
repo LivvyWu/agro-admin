b38ed2dd5f7374b6fa97673cb567793b81e89557
LivvyWu <lulutt0523@gmail.com>
2016-11-07 23:27:26 +0800

    add d3

--- routes/farm.js
+++ routes/farm.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 var metrics =[
-    { "item" : "空氣溫度", "num" : "25" },
-    { "item" : "空氣濕度", "num" : "70" },
-    { "item" : "空氣二氧化碳", "num" : "80" },
-    { "item" : "照度", "num" : "65"} ,
-    { "item" : "土壤溫度", "num" : "20" },
-    { "item" : "土壤濕度", "num" : "60" },
-    { "item" : "土壤電解質", "num" : "70" },
-    { "item" : "土壤酸鹼值", "num" : "5" }
+    { "itemCd" : "InAirTem", "val" : "25", "itemNm" : "空氣溫度" , "icon": "fa fa-thermometer-half"},
+    { "itemCd" : "InAirHumidity", "val" : "75", "itemNm" : "空氣濕度" , "icon": "fa fa-tint"},
+    { "itemCd" : "InAirIllumination", "val" : "40K", "itemNm" : "照度" , "icon": "fa fa-sun-o"},
+    { "itemCd" : "InAirCO2", "val" : "25", "itemNm" : "空氣二氧化碳" , "icon": "fa fa-cloud"},
+    { "itemCd" : "InSoilTemperature", "val" : "20", "itemNm" : "土壤溫度" , "icon": "fa fa-thermometer-full"},
+    { "itemCd" : "InSoilHumidity", "val" : "90", "itemNm" : "土壤濕度" , "icon": "fa fa-tint"},
+    { "itemCd" : "InSoilEC", "val" : "25", "itemNm" : "土壤電解質" , "icon": "fa fa-tachometer"},
+    { "itemCd" : "InSoilPH", "val" : "5", "itemNm" : "土壤酸鹼值" , "icon": "fa fa-flask"}
 ];
 
 
