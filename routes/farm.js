d7dcf09cbe6484889c50be984a51367ac2aa3add
LivvyWu <livvy@LivvydeMacBook-Air.local>
2016-11-08 11:18:06 +0800

    濕度

--- routes/farm.js
+++ routes/farm.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 var metrics =[
-    { "itemCd" : "InAirTem", "val" : "25", "itemNm" : "空氣溫度" , "icon": "fa fa-thermometer-half"},
-    { "itemCd" : "InAirHumidity", "val" : "75", "itemNm" : "空氣濕度" , "icon": "fa fa-tint"},
-    { "itemCd" : "InAirIllumination", "val" : "40K", "itemNm" : "照度" , "icon": "fa fa-sun-o"},
-    { "itemCd" : "InAirCO2", "val" : "25", "itemNm" : "空氣二氧化碳" , "icon": "fa fa-cloud"},
-    { "itemCd" : "InSoilTemperature", "val" : "20", "itemNm" : "土壤溫度" , "icon": "fa fa-thermometer-full"},
-    { "itemCd" : "InSoilHumidity", "val" : "90", "itemNm" : "土壤濕度" , "icon": "fa fa-tint"},
-    { "itemCd" : "InSoilEC", "val" : "25", "itemNm" : "土壤電解質" , "icon": "fa fa-tachometer"},
-    { "itemCd" : "InSoilPH", "val" : "5", "itemNm" : "土壤酸鹼值" , "icon": "fa fa-flask"}
+    { "itemCd" : "InAirTem", "val" : "25", "itemNm" : "空氣溫度" , "icon": "fa fa-thermometer-half", "link" : "datachart/1"},
+    { "itemCd" : "InAirHumidity", "val" : "75", "itemNm" : "空氣濕度" , "icon": "fa fa-tint", "link" : "datachart/2"},
+    { "itemCd" : "InAirIllumination", "val" : "40K", "itemNm" : "照度" , "icon": "fa fa-sun-o", "link" : "datachart/1"},
+    { "itemCd" : "InAirCO2", "val" : "25", "itemNm" : "空氣二氧化碳" , "icon": "fa fa-cloud", "link" : "datachart/2"},
+    { "itemCd" : "InSoilTemperature", "val" : "20", "itemNm" : "土壤溫度" , "icon": "fa fa-thermometer-full", "link" : "datachart/1"},
+    { "itemCd" : "InSoilHumidity", "val" : "90", "itemNm" : "土壤濕度" , "icon": "fa fa-tint", "link" : "datachart/2"},
+    { "itemCd" : "InSoilEC", "val" : "25", "itemNm" : "土壤電解質" , "icon": "fa fa-tachometer", "link" : "datachart/1"},
+    { "itemCd" : "InSoilPH", "val" : "5", "itemNm" : "土壤酸鹼值" , "icon": "fa fa-flask", "link" : "datachart/2"}
 ];
 
 
