e381af61ec24ac536ab640a1323d43921a6b841e
wupengta <wupengta@gmail.com>
2016-11-10 13:32:36 +0800

    Register

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var Dummy = require('./dummy_user');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' });
@@ -11,10 +13,15 @@ router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Express' });
 });
 
+
 /* GET home page. */
 router.post('/login/submit', function(req, res, next) {
 
   console.log(req.body.password);
+
+  var users = new Dummy();
+  console.log("dummy:" + users.dummuyUsers);
+
 
   res.redirect("/farm");
 });
